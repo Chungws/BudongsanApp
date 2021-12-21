74927d990cf7a48d54a37a37df7779b560d4e84f
woosung <jungyh0528@gmail.com>
2021-12-21 22:22:14 +0900

    Upload Available HWP and PDF files

--- src/components/imageUploadModal.js
+++ src/components/imageUploadModal.js
@@ -2,15 +2,24 @@ import React, { useState } from "react";
 import ImageUploader from "react-images-upload";
 
 const ImageUploadModal = (props) => {
-  const { getImages } = props;
+  const { getImages, getDocuments } = props;
   const [pictures, setPictures] = useState([]);
+  const [documents, setDocuments] = useState([]);
 
   React.useEffect(() => {
     getImages(pictures[pictures.length-1])
-  },[pictures])
+  }, [pictures])
 
-  const onDrop = picture => {
-    setPictures([...pictures, picture])
+  React.useEffect(() => {
+    getDocuments(documents[documents.length-1])
+  }, [documents])
+
+  const onDrop = data => {
+    if (data.type === 'application/pdf' || data.type === 'application/haansofthwp') {
+      setDocuments([...documents, data])
+    } else {
+      setPictures([...pictures, data])
+    }
   }
 
   return (
@@ -19,11 +28,11 @@ const ImageUploadModal = (props) => {
       withIcon={true}
       withPreview={true}
       buttonText={"사진 업로드"}
-      label={"최대 파일 크기 : 10MB, 가능한 확장자 : jpg, gif, png, heic, jpeg, jfif"}
+      label={"최대 파일 크기 : 10MB, 가능한 확장자 : jpg, gif, png, heic, jpeg, jfif, pdf, hwp"}
       onChange={onDrop}
       fileSizeError={"파일 크기가 너무 큽니다"}
       fileTypeError={"파일의 확장자가 다릅니다"}
-      imgExtension={[".jpg", ".gif", ".png", ".heic", ".jpeg", ".jfif"]}
+      imgExtension={[".jpg", ".gif", ".png", ".heic", ".jpeg", ".jfif", ".pdf", ".hwp"]}
       maxFileSize={10_000_000}
     />
   );
